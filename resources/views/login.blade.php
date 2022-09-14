59d6f518f49e9e1709cc1e6780033ffc6aeb2040
ireneusz.piskorski <ireneusz.piskorski@le-campus-numerique.fr>
2022-09-14 14:19:51 +0200

    2.1 — Mise en place des URL dans votre fichier de route

---
+++ resources/views/login.blade.php
@@ -0,0 +1,20 @@
+<?php
+?>
+
+    <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="/style.css" rel="stylesheet">
+    <title>Login</title>
+</head>
+<body>
+<h1>Login</h1>
+
+<a href="/">Home</a>
+<br>
+<a href="/cart">Cart</a>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,32 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+//Route::get('/', function () {
+//    return view('home');
+//});
+//
+//Route::get('cart', function () {
+//    return view('cart');
+//});
+//
+//Route::get('login', function () {
+//    return view('login');
+//});
+
+
 Route::get('/', function () {
-    return view('welcome');
+    return 'Homepage';
 });
+
+Route::get('product', function () {
+    return 'Liste des produits';
+});
+
+Route::get('/product/{id}', function ($id) {
+    return 'Fiche du produit ' . $id;
+})->where('id', '[0-9]+');
+
+Route::get('cart', function () {
+    return 'Panier';
+});
+
